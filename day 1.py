0fef807513808ca0f0918336d21ef8c54e539d0e
Shaine-San <53440579+Shaine-San@users.noreply.github.com>
2019-07-31 10:10:25 -0400

    Add files via upload

---
+++ day 1.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+Hello my name is shiane
+
+print("my name is shaine")
+
+print("i want food")
+
+5/2
+
+5*2
+
+5-2
+
+5^2
+
+8/9*2
+
+print("5*2") 
+
+def mutiply(a,b):
+    mutiply=a*b
+    
+    print(mutiply)
+    
+multiply(4,5)
+
+def mutiply (2*5):
+    mutiply=2*10
+    
+    (print)
+ 
+Area=1/2*base *heihght
+Area=1/2*5*6
+print 1/2*5*6
+ 
+
+The area of trianle with
+base 2 and heght 2 is
+
+
---
+++ day 2.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 30 10:50:29 2019
+
+@author: STEM
+"""
+
+def cadbury(a):
+    cadbury=a+10
+    print(cadbury)
+    mn m  
+cadbury(5)
+cadbury=(5)cadbury=cadbury+10
+print(cadburyBox)
+
+print Cadbury="Milk Chocolate"
+print cadbury= "Dark Chocolate"
+print 
+
+def cadbury (w,d,m):
+    print("There is", m,",d",",w,"in the box")
+     
+    cadbury("White Chocolate","Dark Chocolate,"Mike Chocolate")
+
+name=input("please ener your name")
+print(Your name is",name)
+
+print your na
+
+ageint=int(age)
+ageint
+
+import math
+dir(math)
+
+math.pi
+
+mat.gi
+
+mathh.factorial(0)
+
+6 pow.15
+
+math.pow(6,15)
+
+the cubic root of the dog is 12
